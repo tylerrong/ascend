34e5c35ac07aeb1d40e82322ac40dcfb0d74aea6
tylerrong <tylerrong613@gmail.com>
2024-12-01 13:05:53 -0800

    Initial Commit

---
+++ RewardsTracker/RewardsTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  RewardsTrackerApp.swift
+//  RewardsTracker
+//
+//  Created by Tyler Rong on 12/1/24.
+//
+
+import SwiftUI
+
+@main
+struct RewardsTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RewardsTrackerTests/RewardsTrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  RewardsTrackerTests.swift
+//  RewardsTrackerTests
+//
+//  Created by Tyler Rong on 12/1/24.
+//
+
+import Testing
+@testable import RewardsTracker
+
+struct RewardsTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
